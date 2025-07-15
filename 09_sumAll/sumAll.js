6b16c0a5cba4cb31faaee05f59dc040a74b648c0
NuttyNut2299 <199890687+NuttyNut2299@users.noreply.github.com>
2025-07-15 14:59:57 +0800

    Completion of some javascript exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    if (typeof firstNum === 'number' && firstNum > 0 && Number.isInteger(firstNum) && 
+        typeof secondNum === 'number' && secondNum > 0 && Number.isInteger(secondNum)) {
+        let sum = 0;
+        if (firstNum > secondNum) {
+            let acc = secondNum;
+            for (let i = 0; i < (firstNum - secondNum + 1); i++){
+                sum+=acc;
+                acc+=1;
+            }
+        } else {
+            let acc = firstNum;
+            for (let i = 0; i < (secondNum - firstNum + 1); i++){
+                sum+=acc;
+                acc+=1;
+            }
+        }
+        return sum;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(valueF) {
+  return Math.round(((valueF - 32) * (5 / 9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(valueC) {
+  return Math.round(((valueC * (9 / 5)) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
